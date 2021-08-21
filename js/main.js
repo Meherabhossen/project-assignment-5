b111d843bc9cde3fa22c55d7074255b532d08d8c
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2021-08-21 22:35:36 +0600

    last commit

--- js/main.js
+++ js/main.js
@@ -17,6 +17,89 @@ document.getElementById('btn-16GB').addEventListener('click',function(){
     lastTotal.innerText = 1299 + memoryCost; 
 });
 
-//////storage
 
-document.getElementById('storage-256GB')
+//// Storage 
+
+document.getElementById('storage-256GB').addEventListener('click',function(){
+    const storage256= document.getElementById('strong-cost');
+    storage256.innerText = 0;
+    const totalCost = document.getElementById('total-cost');
+    totalCost.innerText = 1299;
+    const lastTotal = document.getElementById('last-total');
+    lastTotal.innerText = 1299;  
+});
+
+document.getElementById('storage-512GB').addEventListener('click',function(){
+    const storage512 = document.getElementById('strong-cost');
+    const memoryCost = storage512.innerText = 100;
+    const totalCost = document.getElementById('total-cost');
+    totalCost.innerText = 1299 + memoryCost;
+    const lastTotal = document.getElementById('last-total');
+    lastTotal.innerText = 1299 + memoryCost; 
+});
+
+document.getElementById('storage-1TbGB').addEventListener('click',function(){
+    const storage1TB = document.getElementById('strong-cost');
+    const memoryCost = storage1TB.innerText = 180;
+    const totalCost = document.getElementById('total-cost');
+    totalCost.innerText = 1299 + memoryCost;
+    const lastTotal = document.getElementById('last-total');
+    lastTotal.innerText = 1299 + memoryCost; 
+});
+
+
+
+///// Delivery
+
+
+
+
+document.getElementById('free-delivery').addEventListener('click',function(){
+    const deliverWithoutCost = document.getElementById('delivery-cost');
+    const delyveryCost = deliverWithoutCost.innerText = 0;
+
+    const totalCost = document.getElementById('total-cost');
+    totalCost.innerText = 1299 + delyveryCost;
+    const lastTotal = document.getElementById('last-total');
+    lastTotal.innerText = 1299 + delyveryCost; 
+});
+
+document.getElementById('paid-delivery').addEventListener('click',function(){
+    const deliverWithCost = document.getElementById('delivery-cost');
+    const delyveryCost = deliverWithCost.innerText = 20;
+
+    const totalCost = document.getElementById('total-cost');
+    totalCost.innerText = 1299 + delyveryCost;
+    const lastTotal = document.getElementById('last-total');
+    lastTotal.innerText = 1299 + delyveryCost; 
+});
+
+///// Coupon section
+
+function pomoCode(){
+    
+
+        const totalCost =document.getElementById('total-cost');
+        const totalValue = totalCost.innerText;
+
+        const lastTotal = document.getElementById('last-total');
+        const lastTotalValue = lastTotal.innerText;
+
+        const pomoCode = document.getElementById('pomo-code').value;
+        
+
+        if (pomoCode === 'stevekaku') {
+
+            const afterCoupon =  totalValue - (totalValue*0.2);
+           totalCost.innerText = parseInt(afterCoupon);
+
+           const afterCouponLast = lastTotalValue-(lastTotalValue*0.2);
+           lastTotal.innerText = parseInt(afterCouponLast);
+
+
+            event.preventDefault();
+            document.getElementById('pomo-code').value=" ";
+        }
+
+   
+}
